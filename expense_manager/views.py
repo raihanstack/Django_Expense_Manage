304a70bbf2db7e459b7a8b44700ec25cf15b493c
Mostafa Raihan <raihan.invite@gmail.com>
2026-02-16 21:54:59 +0600

    add html

--- expense_manager/views.py
+++ expense_manager/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Expense
 
 def user_login(request):
     return render(request, 'login.html')
 
 def user_loginout(request):
-    pass
+    return redirect('login')
 
 def user_register(request):
     return render(request, 'register.html')
@@ -22,11 +23,12 @@ def password_reset_complete(request):
     return render(request, 'password_reset_complete.html')
 
 
+
 def home(request):
     return render(request, 'home.html')
 
 def expense_list(request):
-    return render(request, 'home.html')
+    return render(request, 'expense_list.html')
 
 def expense_create(request):
     return render(request, 'expense_form.html')
@@ -34,5 +36,9 @@ def expense_create(request):
 def expense_update(request):
     return render(request, 'expense_form.html')
 
-def expense_delete(request):
-    pass
+def expense_delete(request, id):
+    expense = get_object_or_404(Expense, id=id)
+    if request.method == 'POST':
+        expense.delete()
+        return redirect('expense_list')
+    return render(request, 'expense_confirm_delete.html', {'expense': expense})
