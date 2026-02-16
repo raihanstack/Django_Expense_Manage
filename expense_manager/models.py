4672e56cd2e3097703293ca544c3530bae34c039
Mostafa Raihan <raihan.invite@gmail.com>
2026-02-16 21:01:29 +0600

    add template

--- expense_manager/models.py
+++ expense_manager/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name=models.CharField(max_length=100,unique=True)
+
+class Expense(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    Category=models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    amount=models.DecimalField(max_digits=10, decimal_places=2)
+    description=models.TextField(blank=True, null=True)
+    date=models.DateField(auto_now_add=True)
