f8b4cc4947b002f50cf9823ed0fb994a9307fa8d
Mostafa Raihan <raihan.invite@gmail.com>
2026-02-17 08:20:57 +0600

    login form

--- expense_manager/views.py
+++ expense_manager/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Expense
 from django.utils import timezone
 from datetime import timedelta
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 def user_login(request):
     return render(request, 'login.html')
@@ -10,7 +12,33 @@ def user_loginout(request):
     return redirect('login')
 
 def user_register(request):
+
+    if request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password1 = request.POST.get("password1")
+        password2 = request.POST.get("password2")
+
+        if password1 != password2:
+            messages.error(request, "Password do not match!")
+
+        elif User.objects.filter(username=username).exists():
+            messages.error(request, "Username Already Taken!")
+
+        elif User.objects.filter(email=email).exists():
+            messages.error(request, "Email Already Registered!")
+
+        else:
+            User.objects.create_user(
+                username=username,
+                email=email,
+                password=password1
+            )
+            messages.success(request, "Account Created Successfully!")
+            return redirect("login")
+
     return render(request, 'register.html')
+
 
 def password_reset_request(request):
     return render(request, 'password_reset.html')
